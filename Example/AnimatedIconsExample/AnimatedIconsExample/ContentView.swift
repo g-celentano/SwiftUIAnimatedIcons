052c9f4be1e26cd78f3b25abb55ca0cacfd8972f
g-celentano <gaetano99celentano@gmail.com>
2023-08-22 19:01:29 +0200

    updated example

--- Example/AnimatedIconsExample/AnimatedIconsExample/ContentView.swift
+++ Example/AnimatedIconsExample/AnimatedIconsExample/ContentView.swift
@@ -9,11 +9,14 @@ import AnimatedIcons
 import SwiftUI
 
 struct ContentView: View {
-    @State var isSelected: Bool = false
+    @State var isSelectedVertical: Bool = false
+    @State var isSelectedHorizontal: Bool = false
     var body: some View {
         VStack {
-            MoreVert_Cross(isSelected: $isSelected, firstColor: .blue, secondColor: .red, thirdColor: .yellow, crossColor: .green)
-            Text(isSelected ? "cross" : "more vert")
+            MoreVert_Cross(isSelected: $isSelectedVertical, firstColor: .blue, secondColor: .red, thirdColor: .yellow, crossColor: .green)
+            Text(isSelectedVertical ? "cross" : "more vertical")
+            MoreHor_Cross(isSelected: $isSelectedHorizontal, firstColor: .brown, secondColor: .indigo, thirdColor: .purple)
+            Text(isSelectedHorizontal ? "cross" : "more horizontal")
         }
         .padding()
     }
