f55ba15ca7f34f3a1fa9dd925850ddaa584e0ded
g-celentano <gaetano99celentano@gmail.com>
2023-08-25 18:59:35 +0200

    corrected sun_moon animation sequence, added 'modular' sunRays shapes

--- Sources/AnimatedIcons/Sun_Moon.swift
+++ Sources/AnimatedIcons/Sun_Moon.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+public enum SunRayShape {
+    case circle
+    case roundedRectangle
+    case triangle
+}
+
 /// Sun-Moon icon
 /// from sun to moon
 /// Parameters list:
@@ -18,12 +24,13 @@ import SwiftUI
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public struct Sun_Moon: View {
-    public init(_ isSun: Binding<Bool>, size: CGFloat = 24.0, duration: CGFloat = 0.35, sunColor: Color = .black, moonColor: Color = .black) {
+    public init(_ isSun: Binding<Bool>, size: CGFloat = 24.0, duration: CGFloat = 0.35, sunColor: Color = .black, moonColor: Color = .black, sunRayShape: SunRayShape = .roundedRectangle) {
         _isSun = isSun
         self.size = size
         self.duration = duration
         self.sunColor = sunColor
         self.moonColor = moonColor
+        self.sunRaysShape = sunRayShape
     }
 
     @Binding private var isSun: Bool
@@ -31,8 +38,9 @@ public struct Sun_Moon: View {
     @State private var duration: CGFloat
     @State private var sunColor: Color
     @State private var moonColor: Color
+    @State private var sunRaysShape: SunRayShape
 
-    @State private var sunDotsOutside: Bool = false
+    @State private var sunDotsOutside: Bool = true
 
     public var body: some View {
         ZStack {
@@ -43,10 +51,25 @@ public struct Sun_Moon: View {
             ForEach(0 ..< 8, id: \.self) { index in
                 let xOffset = roundingCirclesDistance * cos(.pi / 4 * CGFloat(index))
                 let yOffset = roundingCirclesDistance * sin(.pi / 4 * CGFloat(index))
-                Circle()
-                    .frame(width: roundingCirlcesRadii)
-                    .offset(x: xOffset, y: yOffset)
-                    .foregroundColor(sunColor)
+                if sunRaysShape == .circle{
+                    Circle()
+                        .frame(width: roundingCirlcesRadii, height: roundingCirlcesRadii)
+                        .offset(x: xOffset, y: yOffset)
+                        .foregroundColor(sunColor)
+                } else if sunRaysShape == .roundedRectangle {
+                    RoundedRectangle(cornerRadius: roundingCirlcesRadii * 0.1)
+                        .frame(width: roundingCirlcesRadii * 0.9, height: roundingCirlcesRadii * 0.3)
+                        .rotationEffect(.degrees(CGFloat(index) * 45.0), anchor: .center)
+                        .offset(x: xOffset, y: yOffset)
+                        .foregroundColor(sunColor)
+                } else if sunRaysShape == .triangle {
+                    SunRay()
+                        .frame(width: roundingCirlcesRadii, height: roundingCirlcesRadii)
+                        .rotationEffect(.degrees(CGFloat(index) * 45.0), anchor: .center)
+                        .rotationEffect(.degrees(90))
+                        .offset(x: xOffset, y: yOffset)
+                        .foregroundColor(sunColor)
+                }
             }
 
             ZStack {
@@ -68,10 +91,8 @@ public struct Sun_Moon: View {
                     sunDotsOutside = false
                 }
                 DispatchQueue.main.asyncAfter(deadline: .now() + duration * 0.5) {
-                    print("after \(duration * 0.5)")
                     withAnimation(.linear(duration: duration * 0.5)) {
                         isSun = false
-                        print("second animation -- \(isSun)")
                     }
                 }
             } else {
@@ -79,10 +100,8 @@ public struct Sun_Moon: View {
                     isSun = true
                 }
                 DispatchQueue.main.asyncAfter(deadline: .now() + duration * 0.5) {
-                    print("after \(duration * 0.5)")
                     withAnimation(.linear(duration: duration * 0.5)) {
                         sunDotsOutside = true
-                        print("second animation \(sunDotsOutside)")
                     }
                 }
             }
@@ -92,8 +111,21 @@ public struct Sun_Moon: View {
 
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
+private struct SunRay: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.closeSubpath()
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+@available(macOS 10.15, *)
 struct SunMoon_Previews: PreviewProvider {
     static var previews: some View {
-        Sun_Moon(.constant(false), size: 150)
+        Sun_Moon(.constant(true), size: 150, sunRayShape: .triangle)
     }
 }
