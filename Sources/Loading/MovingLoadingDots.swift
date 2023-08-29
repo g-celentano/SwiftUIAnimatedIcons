e281b55d0d582b50cc69c7b5639de4c5f0afde31
g-celentano <gaetano99celentano@gmail.com>
2023-08-29 13:32:05 +0200

    made moving dots animation editable

--- Sources/Loading/MovingLoadingDots.swift
+++ Sources/Loading/MovingLoadingDots.swift
@@ -21,15 +21,19 @@ import SwiftUI
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public struct MovingLoadingDots: View {
-    public init(size: CGFloat = 48, color: Color = .black, dotsShape: DotShape = .circle) {
+    public init(size: CGFloat = 48, color: Color = .black, dotsShape: DotShape = .circle, animationDuration: CGFloat = 0.2, isAnimationDelayed: Bool = true) {
         self.size = size
         self.color = color
         self.dotsShape = dotsShape
+        self.animationDuration = animationDuration
+        self.isAnimationDelayed = isAnimationDelayed
     }
 
     @State private var size: CGFloat
     @State private var color: Color
     @State private var dotsShape: DotShape
+    @State private var animationDuration: CGFloat
+    @State private var isAnimationDelayed: Bool
 
     @State private var movingAmount: CGFloat = 0.0
     @State private var scale: CGFloat = 1.0
@@ -45,25 +49,25 @@ public struct MovingLoadingDots: View {
                 .clipShape(ActualDotShape(shapeToDraw: dotsShape))
                 .scaleEffect(1 - scale, anchor: appearingPoint)
                 .offset(x: (-size * 0.6) + movingAmount, y: 0)
-                .animation(.linear(duration: 0.25).delay(0.2).repeatForever(autoreverses: false), value: movingAmount)
-                .animation(.linear(duration: 0.25).repeatForever(autoreverses: false), value: scale)
+                .animation(.linear(duration: animationDuration).delay(isAnimationDelayed ? 0.2 : 0.0).repeatForever(autoreverses: false), value: movingAmount)
+                .animation(.linear(duration: animationDuration).repeatForever(autoreverses: false), value: scale)
             Rectangle()
                 .frame(width: dotSize, height: dotSize)
                 .clipShape(ActualDotShape(shapeToDraw: dotsShape))
                 .offset(x: (-size * 0.3) + movingAmount, y: 0)
-                .animation(.linear(duration: 0.25).delay(0.2).repeatForever(autoreverses: false), value: movingAmount)
+                .animation(.linear(duration: animationDuration).delay(isAnimationDelayed ? 0.2 : 0.0).repeatForever(autoreverses: false), value: movingAmount)
             Rectangle()
                 .frame(width: dotSize, height: dotSize)
                 .clipShape(ActualDotShape(shapeToDraw: dotsShape))
                 .offset(x: movingAmount, y: 0)
-                .animation(.linear(duration: 0.25).delay(0.2).repeatForever(autoreverses: false), value: movingAmount)
+                .animation(.linear(duration: animationDuration).delay(isAnimationDelayed ? 0.2 : 0.0).repeatForever(autoreverses: false), value: movingAmount)
             Rectangle()
                 .frame(width: dotSize, height: dotSize)
                 .clipShape(ActualDotShape(shapeToDraw: dotsShape))
                 .scaleEffect(scale, anchor: disappearingPoint)
                 .offset(x: size * 0.3 + movingAmount, y: 0)
-                .animation(.linear(duration: 0.25).delay(0.2).repeatForever(autoreverses: false), value: movingAmount)
-                .animation(.linear(duration: 0.25).repeatForever(autoreverses: false), value: scale)
+                .animation(.linear(duration: animationDuration).delay(isAnimationDelayed ? 0.2 : 0.0).repeatForever(autoreverses: false), value: movingAmount)
+                .animation(.linear(duration: animationDuration).repeatForever(autoreverses: false), value: scale)
         }
         .frame(width: size, height: size)
         .foregroundColor(color)
@@ -78,6 +82,6 @@ public struct MovingLoadingDots: View {
 @available(macOS 10.15, *)
 struct MovingLoadingDots_Previews: PreviewProvider {
     static var previews: some View {
-        MovingLoadingDots(dotsShape: .circle)
+        MovingLoadingDots(dotsShape: .circle, isAnimationDelayed: false)
     }
 }
