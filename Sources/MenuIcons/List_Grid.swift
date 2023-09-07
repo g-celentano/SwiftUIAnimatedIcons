47c92b85200e5c3877c03551dd6857695694984a
g-celentano <gaetano99celentano@gmail.com>
2023-09-07 13:41:16 +0200

    updated List_Grid state changes

--- Sources/MenuIcons/List_Grid.swift
+++ Sources/MenuIcons/List_Grid.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+public enum GridLayout {
+    case TwoByTwo
+    case ThreeByTwo
+}
+
 /// List-Gird
 /// Parameters list:
 /// - **isList**: state handle value
@@ -17,58 +22,130 @@ import SwiftUI
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public struct List_Grid: View {
-    public init(_ isList: Binding<Bool>, size: CGFloat = 24.0, color: Color = .black, duration: CGFloat = 0.35) {
+    public init(_ isList: Binding<Bool>, size: CGFloat = 24.0, color: Color = .black, duration: CGFloat = 0.35, gridLayout: GridLayout = .TwoByTwo) {
         _isList = isList
         self.size = size
         self.color = color
         self.duration = duration
+        self.gridLayout = gridLayout
 
-        self.gridCols = isList.wrappedValue ? 2 : 3
+        var offs = [CGPoint](repeating: .zero, count: 6)
+        if isList.wrappedValue {
+            offs[0] = CGPoint(x: -size * 0.35, y: -size * 0.2)
+            offs[1] = CGPoint(x: -size * 0.35, y: 0)
+            offs[2] = CGPoint(x: -size * 0.35, y: size * 0.2)
+            offs[3] = CGPoint(x: size * 0.11, y: -size * 0.2)
+            offs[4] = CGPoint(x: size * 0.11, y: 0)
+            offs[5] = CGPoint(x: size * 0.11, y: size * 0.2)
+        } else {
+            if gridLayout == .TwoByTwo {
+                offs[0] = CGPoint(x: -size * 0.15, y: -size * 0.2)
+                offs[1] = CGPoint(x: -size * 0.15, y: -size * 0.2)
+                offs[2] = CGPoint(x: -size * 0.15, y: size * 0.2)
+                offs[3] = CGPoint(x: size * 0.15, y: -size * 0.2)
+                offs[4] = CGPoint(x: size * 0.15, y: -size * 0.2)
+                offs[5] = CGPoint(x: size * 0.15, y: size * 0.2)
+            } else {
+                offs[0] = CGPoint(x: -size * 0.3, y: -size * 0.2)
+                offs[1] = CGPoint(x: 0, y: -size * 0.2)
+                offs[2] = CGPoint(x: -size * 0.3, y: size * 0.2)
+                offs[3] = CGPoint(x: size * 0.3, y: -size * 0.2)
+                offs[4] = CGPoint(x: 0, y: size * 0.2)
+                offs[5] = CGPoint(x: size * 0.3, y: size * 0.2)
+            }
+        }
+
+        self.offsets = offs
     }
 
     @Binding private var isList: Bool
     @State private var size: CGFloat
     @State private var color: Color
     @State private var duration: CGFloat
+    @State private var gridLayout: GridLayout
 
-    @State private var gridCols: Int
+    @State private var offsets: [CGPoint]
 
     public var body: some View {
         ZStack {
-            // MARK: Left dots to grid elements
-
-            // list top left rectangle - grid top center rectangle
+            // left list dots to grid elements
             Rectangle()
-                .frame(width: isList ? size * 0.1 : size * 0.3, height: isList ? size * 0.1 : size * 0.35)
-                .offset(x: isList ? -size * 0.33 : 0, y: isList ? -size * 0.15 : -size * 0.19)
-            // list center left rectangle - grid top left rectangle
+                .frame(width: isList ? size * 0.15 : size * 0.275, height: isList ? size * 0.15 : size * 0.35)
+                .offset(x: offsets[0].x, y: offsets[0].y)
             Rectangle()
-                .frame(width: isList ? size * 0.1 : size * 0.3, height: isList ? size * 0.1 : size * 0.35)
-                .offset(x: -size * 0.33, y: isList ? 0 : -size * 0.19)
-            // list bottom left rectangle - grid bottom left rectangle
+                .frame(width: isList ? size * 0.15 : size * 0.275, height: isList ? size * 0.15 : size * 0.35)
+                .offset(x: offsets[1].x, y: offsets[1].y)
             Rectangle()
-                .frame(width: isList ? size * 0.1 : size * 0.3, height: isList ? size * 0.1 : size * 0.35)
-                .offset(x: -size * 0.33, y: isList ? size * 0.15 : size * 0.19)
-
-            // MARK: Right long rectangles to grid elements
-
-            // list top right rectangle - grid top right rectangle
+                .frame(width: isList ? size * 0.15 : size * 0.275, height: isList ? size * 0.15 : size * 0.35)
+                .offset(x: offsets[2].x, y: offsets[2].y)
+            // right list lines to grid elements
             Rectangle()
-                .frame(width: isList ? size * 0.5 : size * 0.3, height: isList ? size * 0.1 : size * 0.35)
-                .offset(x: isList ? size * 0.05 : size * 0.33, y: isList ? -size * 0.15 : -size * 0.19)
-            // list center right rectangle - grid bottom right rectangle
+                .frame(width: isList ? size * 0.65 : size * 0.275, height: isList ? size * 0.15 : size * 0.35)
+                .offset(x: offsets[3].x, y: offsets[3].y)
             Rectangle()
-                .frame(width: isList ? size * 0.5 : size * 0.3, height: isList ? size * 0.1 : size * 0.35)
-                .offset(x: isList ? size * 0.05 : size * 0.33, y: isList ? 0 : size * 0.19)
-            // list bottom right rectangle - grid bottom center rectangle
+                .frame(width: isList ? size * 0.65 : size * 0.275, height: isList ? size * 0.15 : size * 0.35)
+                .offset(x: offsets[4].x, y: offsets[4].y)
             Rectangle()
-                .frame(width: isList ? size * 0.5 : size * 0.3, height: isList ? size * 0.1 : size * 0.35)
-                .offset(x: isList ? size * 0.05 : 0, y: isList ? size * 0.15 : size * 0.19)
+                .frame(width: isList ? size * 0.65 : size * 0.275, height: isList ? size * 0.15 : size * 0.35)
+                .offset(x: offsets[5].x, y: offsets[5].y)
         }
         .frame(width: size, height: size)
-        .onTapGesture {
-            withAnimation(.linear(duration: duration)) {
-                isList.toggle()
+        .foregroundColor(color)
+        .onTapGesture(perform: updateOffsets)
+        .background(Color.red)
+    }
+
+    private func updateOffsets() {
+        if isList {
+            withAnimation(.linear(duration: duration * 0.35)) {
+                for i in 0 ..< offsets.count {
+                    offsets[i] = .zero
+                }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + duration * 0.35) {
+                withAnimation(.linear(duration: duration * 0.2)) {
+                    isList = false
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + duration * 0.2) {
+                    withAnimation(.linear(duration: duration * 0.35)) {
+                        if gridLayout == .TwoByTwo {
+                            offsets[0] = CGPoint(x: -size * 0.15, y: -size * 0.2)
+                            offsets[1] = CGPoint(x: -size * 0.15, y: -size * 0.2)
+                            offsets[2] = CGPoint(x: -size * 0.15, y: size * 0.2)
+                            offsets[3] = CGPoint(x: size * 0.15, y: -size * 0.2)
+                            offsets[4] = CGPoint(x: size * 0.15, y: -size * 0.2)
+                            offsets[5] = CGPoint(x: size * 0.15, y: size * 0.2)
+                        } else {
+                            offsets[0] = CGPoint(x: -size * 0.3, y: -size * 0.2)
+                            offsets[1] = CGPoint(x: 0, y: -size * 0.2)
+                            offsets[2] = CGPoint(x: -size * 0.3, y: size * 0.2)
+                            offsets[3] = CGPoint(x: size * 0.3, y: -size * 0.2)
+                            offsets[4] = CGPoint(x: 0, y: size * 0.2)
+                            offsets[5] = CGPoint(x: size * 0.3, y: size * 0.2)
+                        }
+                    }
+                }
+            }
+        } else {
+            withAnimation(.linear(duration: duration * 0.35)) {
+                for i in 0 ..< offsets.count {
+                    offsets[i] = .zero
+                }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + duration * 0.35) {
+                withAnimation(.linear(duration: duration * 0.2)) {
+                    isList = true
+                }
+                DispatchQueue.main.asyncAfter(deadline: .now() + duration * 0.2) {
+                    withAnimation(.linear(duration: duration * 0.35)) {
+                        offsets[0] = CGPoint(x: -size * 0.35, y: -size * 0.2)
+                        offsets[1] = CGPoint(x: -size * 0.35, y: 0)
+                        offsets[2] = CGPoint(x: -size * 0.35, y: size * 0.2)
+                        offsets[3] = CGPoint(x: size * 0.11, y: -size * 0.2)
+                        offsets[4] = CGPoint(x: size * 0.11, y: 0)
+                        offsets[5] = CGPoint(x: size * 0.11, y: size * 0.2)
+                    }
+                }
             }
         }
     }
@@ -78,6 +155,6 @@ public struct List_Grid: View {
 @available(macOS 10.15, *)
 struct List_Grid_Previews: PreviewProvider {
     static var previews: some View {
-        List_Grid(.constant(false), size: 100)
+        List_Grid(.constant(true), size: 100)
     }
 }
